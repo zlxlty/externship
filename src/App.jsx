a4d1eb44f699c729ccd0b38e45d1bc74e4d2c492
zlxlty <lus2@carleton.edu>
2023-11-28 16:42:25 -0600

    change card background color

--- src/App.jsx
+++ src/App.jsx
@@ -34,6 +34,8 @@ function App() {
   const [currentCard, setCurrentCard] = useState(0)
   const [facing, setFacing] = useState('front')
 
+  const cardBackGround = useMemo(() => facing === 'front' ? 'bg-slate-100' : 'bg-slate-400', [facing])
+
   function handleCardClick() {
     facing === 'front' ? setFacing('back') : setFacing('front')
   }
@@ -60,7 +62,7 @@ function App() {
       </div>
 
       <section className='w-screen h-[30vh] flex justify-center items-center'>
-        <div onClick={handleCardClick} className='w-[40vw] h-[30vh] bg-slate-400 text-center flex justify-center items-center text-lg select-none'>
+        <div onClick={handleCardClick} className={`${cardBackGround} w-[40vw] h-[30vh] bg-slate-400 text-center flex justify-center items-center text-lg select-none`}>
           {cards[currentCard][facing]}
         </div>
       </section>
